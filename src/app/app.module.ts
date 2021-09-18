0303ae5cb48b61b41776362bcbe1e6759c11272b
Raz Gaon <razgaon@mit.edu>
2021-09-18 14:32:19 -0400

    Added sidebar and calendar view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+import { IndexDropdownComponent } from "./components/dropdowns/index-dropdown/index-dropdown.component";
+import { TableDropdownComponent } from "./components/dropdowns/table-dropdown/table-dropdown.component";
+import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pages-dropdown.component";
+import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
+import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    IndexDropdownComponent,
+    TableDropdownComponent,
+    PagesDropdownComponent,
+    NotificationDropdownComponent,
+    UserDropdownComponent
   ],
   imports: [
     BrowserModule,
