5abf594069a49eab4e877cf6e4ca5296eaa61690
Raz Gaon <razgaon@mit.edu>
2021-09-18 13:36:44 -0400

    Initial angular project. Added calendar-angular module.

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import { addDays, addHours, startOfDay } from 'date-fns';
+import { colors } from './utils/colors';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  viewDate: Date = new Date();
+
+  events: CalendarEvent[] = [
+    {
+      start: addHours(startOfDay(new Date()), 5),
+      end: addHours(startOfDay(new Date()), 17),
+      title: 'Event 1',
+      color: colors.red,
+      allDay: true,
+    },
+    {
+      start: addHours(startOfDay(addDays(new Date(), 1)), 2),
+      end: addHours(startOfDay(addDays(new Date(), 1)), 18),
+      title: 'Event 2',
+      color: colors.blue,
+      allDay: true,
+    },
+    {
+      start: addHours(startOfDay(new Date()), 8),
+      title: 'Event 3',
+      color: colors.blue,
+      allDay: true,
+    },
+  ];
+}
